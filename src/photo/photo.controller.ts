e65cd0cc41ddb8281ad2fcd53acf06b73ea100cf
Pobx <geidtiphong.s@allrightbox.com>
2022-07-15 10:36:50 +0700

    feat: edit logic for controller

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { PhotoData } from './photo.data';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { InsertPhotoDto } from './insert-photo.dto';
+import { PhotoData, PhotoEntity } from './photo.data';
 import { Photo } from './photo.entity';
+import { UpdatePhotoDto } from './update-photo.dto';
 
 @Controller('photo')
 export class PhotoController {
@@ -9,5 +22,31 @@ export class PhotoController {
   @Get()
   async findAll(): Promise<Photo[]> {
     return await this.photoService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<PhotoEntity> {
+    return await this.photoService.findOne(id);
+  }
+
+  @Post()
+  async create(
+    @Body(new ValidationPipe()) insertPhotoDto: InsertPhotoDto,
+  ): Promise<PhotoEntity> {
+    return await this.photoService.insert(insertPhotoDto);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id,
+    @Body(new ValidationPipe()) updatePhotoDto: UpdatePhotoDto,
+  ): Promise<PhotoEntity> {
+    return this.photoService.update(id, updatePhotoDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async delete(@Param('id', ParseIntPipe) id): Promise<void> {
+    await this.photoService.delete(id);
   }
 }
