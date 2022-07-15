9871057e0e2bae2c6f34d3e8a62a2bb31a880532
Pobx <geidtiphong.s@allrightbox.com>
2022-07-15 10:36:20 +0700

    feat: create photo dto for insert and update

---
+++ src/photo/insert-photo.dto.ts
@@ -0,0 +1,18 @@
+import { IsBoolean, IsInt, IsString } from 'class-validator';
+
+export class InsertPhotoDto {
+  @IsString({ message: 'ใส่ name นำเด้อพี่น้อง' })
+  name: string;
+
+  @IsString({ message: 'ใส่ description นำเด้อพี่น้อง' })
+  description: string;
+
+  @IsString({ message: 'ใส่ filename นำเด้อพี่น้อง' })
+  filename: string;
+
+  @IsInt()
+  views: number;
+
+  @IsBoolean()
+  isPublished: boolean;
+}
