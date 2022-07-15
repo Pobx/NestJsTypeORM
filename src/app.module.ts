3c550199b71f79c5253e685d639383fcbb2d78a7
Pobx <geidtiphong.s@allrightbox.com>
2022-07-15 09:51:08 +0700

    feat: remove not used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 import { PhotoModule } from './photo/photo.module';
 
 @Module({
-  imports: [PhotoModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    PhotoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
